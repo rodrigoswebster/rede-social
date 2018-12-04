3d897e4958b8b07fd2f0cc09c7f95871dc154a3c
Acácio <tecnico2017>
2018-12-04 21:22:32 -0200

    atualiza rede social

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyBLbTUIGh3suKsXek_PsoGk0i94lhdw_Jc",
+    authDomain: "login-app-senac.firebaseapp.com",
+    databaseURL: "https://login-app-senac.firebaseio.com",
+    projectId: "login-app-senac",
+    storageBucket: "login-app-senac.appspot.com",
+    messagingSenderId: "672011311043"
+  };
